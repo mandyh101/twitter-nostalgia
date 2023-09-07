ed265cb8d6f23360b5ca75606a311628e7d40709
mandy <mandyhale10@gmail.com>
2023-09-08 10:24:16 +1200

    setting up navigation items in left section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,69 @@
 import React from 'react'
+import {HiOutlineHashtag} from 'react-icons/hi'
+import {HiEnvelope} from 'react-icons/hi2'
+import { BiHomeCircle, BiUser } from "react-icons/bi";
+import {
+  BsBell,
+  BsBookmark,
+  BsTwitter,
+  BsEnvelope,
+  BsThreeDots,
+} from "react-icons/bs";
+import Link from '../../node_modules/next/link'
+
+// TODO update icons
+const NAVIGATION_ITEMS = [
+  {
+    title: 'Home',
+    icon: BiHomeCircle
+  },
+  {
+    title: 'Notifications',
+    icon: BsBell
+  },
+  {
+    title: 'Explore',
+    icon: HiOutlineHashtag
+  },
+  {
+    title: 'Messages',
+    icon: HiEnvelope
+  },
+  {
+    title: 'Bookmarks',
+    icon: BsBookmark
+  },
+  {
+    title: 'Profile',
+    icon: BiUser
+  },
+  {
+    title: 'More',
+    icon: BsThreeDots
+  },
+]
 
 const Home = () => {
   return (
     <div className='w-full h-full justify-center items-center relative bg-black'>
       <div className='max-w-screen-lg w-full h-full flex relative'>
-        <section>Left section</section>
+        {/* Left side bar */}
+        <section className='max-w-screen-lg w-full h-full flex relative'>
+          <div>
+            {
+              NAVIGATION_ITEMS.map((item) => (
+                <Link className="bg-white/50 flex items-center justify-centerspace-x-2 rounded-3xl px-4 py-3" href={`/${item.title.toLowerCase()}`} key={item.title}>
+                  <div>
+                    <item.icon />
+                  </div>
+                  <div>
+                    {item.title}
+                  </div>
+                </Link>
+              ))
+            }
+          </div>
+        </section>
         <main>Left section</main>
         <section>Right section</section>
       </div>
