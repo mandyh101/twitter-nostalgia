2cd8e3be4cdefd8ac201e8cff3fb8a5839268648
mandy <mandyhale10@gmail.com>
2023-09-17 18:00:34 +1200

    set up tweet view and make home sticky and blur over text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsDot } from 'react-icons/bs'
 import LeftSideBar from './components/LeftSideBar'
 
 const Home = () => {
@@ -7,7 +8,7 @@ const Home = () => {
       <div className='max-w-screen-xl w-full h-full flex relative'>
         <LeftSideBar />
         <main className='ml-[275px] flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600'>
-          <h1 className='3xl font-bold my-4 px-4'>Home</h1>
+          <h1 className='3xl font-bold p-6 text-white backdrop-blur bg-white/10 sticky top-0'>Home</h1>
           <div className='flex items-stretch space-x-2 px-4 py-4 border-t-[0.5px] border-b-[0.5px] border-gray-600 relative'>
             <div className='w-10 h-10 bg-slate-400 rounded-full flex-none'></div>
             <div className='flex flex-col w-full h-full'>
@@ -34,20 +35,34 @@ const Home = () => {
           <div className='flex flex-col'>
             {
               Array.from({length:5}).map((_,i) => (
-                <div key={i} className="border-t-[0.5px] border-b-[0.5px] flex space-x-4">
+                <div key={i} className="flex space-x-4 p-4 border-b-[0.5px]">
                   {/* Tweeter's avatar on left */}
                   <div>
-                    <div className='w-10 h-10 bg-slate-200 rounded-full'></div>
+                    <div className='w-10 h-10 p-4 bg-slate-200 rounded-full'></div>
                   </div>
                   {/* Rest of tweet content on right  */}
-                  <div>
+                  <div className='flex flex-col space-y-4'>
+                    {/* user details */}
                     <div className='flex items-center space-x-1 text-white'>
-                      <div>Name of account</div>
+                      <div className='font-bold'>Name of account</div>
                       <div>@username</div>
+                      <BsDot />
                       <div>4h</div> {/* TODO make dynamic - time since tweet was published  */}
                     </div>
-                    <div></div>
-                    <div></div>
+                    {/* tweet text */}
+                    <div className='text-white text-sm'>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque officia dignissimos fuga quia voluptates consectetur consequatur eveniet sed cumque error ullam aliquam debitis officiis, maxime nesciunt nihil voluptate aut a, quas neque cum earum provident aperiam?
+                    </div>
+                    <div className='bg-slate-400 aspect-square w-full h-96 rounded-xl'>
+                      {/* square placeholder for tweet media */}
+                    </div>
+                    <div className='flex items-center space-x-2 w-full text-white'>
+                      <div>C</div>
+                      <div>R</div>
+                      <div>L</div>
+                      <div>S</div>
+                      <div>SH</div>
+                    </div>
                   </div>
                 </div>
               ))
