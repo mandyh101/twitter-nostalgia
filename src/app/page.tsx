83317024fb814e63f4a6756f35811a84c3a96d90
mandy <mandyhale10@gmail.com>
2023-09-17 18:10:54 +1200

    black blur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className='max-w-screen-xl w-full h-full flex relative'>
         <LeftSideBar />
         <main className='ml-[275px] flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600'>
-          <h1 className='3xl font-bold p-6 text-white backdrop-blur bg-white/10 sticky top-0'>Home</h1>
+          <h1 className='3xl font-bold p-6 text-white backdrop-blur bg-black/10 sticky top-0'>Home</h1>
           <div className='flex items-stretch space-x-2 px-4 py-4 border-t-[0.5px] border-b-[0.5px] border-gray-600 relative'>
             <div className='w-10 h-10 bg-slate-400 rounded-full flex-none'></div>
             <div className='flex flex-col w-full h-full'>
