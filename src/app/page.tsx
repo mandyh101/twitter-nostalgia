6d3c341fed6a83934d2b3723a4a013ec12dd6094
mandy <mandyhale10@gmail.com>
2023-09-19 20:22:28 +1200

    WIP search in right section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsSearch } from 'react-icons/bs'
 import LeftSideBar from './components/LeftSideBar'
 import MiddleSection from './components/MiddleSection'
 
@@ -8,8 +9,21 @@ const Home = () => {
       <div className='max-w-screen-xl w-full h-full flex relative'>
         <LeftSideBar />
         <MiddleSection/>
-        
-        <section></section>
+        <section className='fixed flex flex-col space-y-4 right-0'>
+          <div className='w-full h-full relative space-x-4'>
+            <label htmlFor='searchBox' className='absolute top-0 left-0 h-full flex items-center justify-center'>
+              <BsSearch className='w-4 h-4 text-gray-500' title='Search Twitter'/>
+            </label>
+            <input 
+              id='searchbox'
+              type="text" 
+              placeholder='Search' 
+              className='outline-none bg-transparent w-full h-full rounded-xl px-4 py-8 '/>
+          </div>
+          <div></div>
+          <div></div>
+
+        </section>
       </div>
     </div>
   )
