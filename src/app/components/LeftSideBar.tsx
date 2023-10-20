1b8fec414fdd1730c14821f123e81a4aed13f661
mandy <mandyhale10@gmail.com>
2023-10-20 15:49:26 +1300

    messing with making the left side bar fixed

--- src/app/components/LeftSideBar.tsx
+++ src/app/components/LeftSideBar.tsx
@@ -51,36 +51,37 @@ const NAVIGATION_ITEMS = [
 function LeftSideBar() {
   return (
     <>
-      <section className='sticky left-0 w-[20%] flex flex-col h-screen items-stretch mx-2'>
-        <div className='flex flex-col items-stretch h-full space-y-4 mt-4'>
-          {
-            NAVIGATION_ITEMS.map((item) => (
-              <Link className="hover:bg-white/10 text-white transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl py-2 px-4" href={`/${item.title.toLowerCase()}`} key={item.title}>
-                <div>
-                  <item.icon />
+      <section className='relative left-0 w-[20%] flex flex-col h-screen items-stretch mx-2'>
+        <div className='fixed'>
+          <div className='flex flex-col items-stretch h-full space-y-4 mt-4'>
+            {
+              NAVIGATION_ITEMS.map((item) => (
+                <Link className="hover:bg-white/10 text-white transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl py-2 px-4" href={`/${item.title.toLowerCase()}`} key={item.title}>
+                  <div>
+                    <item.icon />
+                  </div>
+                  {item.title != 'Twitter' && <div>{item.title}</div>}
+                </Link>
+              ))
+            }
+            <button className='rounded-full p-2.5 bg-twitterColor text-lg text-center hover:bg-opacity-70 transition duration-200 w-full'>
+              Tweet
+            </button>
+          </div>
+          <div>
+            <button className='flex items-center w-full rounded-full bg-transparent p-3 text-xs text-center hover:bg-white/10 transition duration-200 justify-between text-white'>
+              <div className='flex items-center space-x-2'>
+                <div className='rounded-full bg-slate-400 w-8 h-8'></div>
+                <div className='text-left text-xs'>
+                  <div className='font-semibold'>mandyCodes</div>
+                  <div className=''>@mandycodes30</div>
                 </div>
-                {item.title != 'Twitter' && <div>{item.title}</div>}
-      
-              </Link>
-            ))
-          }
-          <button className='rounded-full p-2.5 bg-twitterColor text-lg text-center hover:bg-opacity-70 transition duration-200 w-full'>
-            Tweet
-          </button>
-        </div>
-        <div>
-          <button className='flex items-center w-full rounded-full bg-transparent p-3 text-xs text-center hover:bg-white/10 transition duration-200 justify-between text-white'>
-            <div className='flex items-center space-x-2'>
-              <div className='rounded-full bg-slate-400 w-8 h-8'></div>
-              <div className='text-left text-xs'>
-                <div className='font-semibold'>mandyCodes</div>
-                <div className=''>@mandycodes30</div>
               </div>
-            </div>
-            <div>
-              <BsThreeDots/>
-            </div>
-          </button>
+              <div>
+                <BsThreeDots/>
+              </div>
+            </button>
+          </div>
         </div>
       </section>
     </>
---
+++ src/app/components/RightSideBar.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { BsSearch } from 'react-icons/bs'
+
+
+function RightSideBar() {
+  return (
+    <section className='fixed right-0 mt-2 flex flex-col items-stretch px-6 w-[30%]'>
+            <div>
+              <div className='relative w-full h-full'>
+                <input
+                  id='searchbox'
+                  type="text"
+                  placeholder='Search'
+                  className='outline-none bg-neutral-900 w-full h-full rounded-full px-10 py-3 border border-transparent focus:border-twitterColor peer focus:bg-gray-50'
+                />
+                <label 
+                  htmlFor='searchBox' 
+                  className='absolute top-0 left-0 h-full flex items-center justify-center p-4 text-gray-500 peer-focus:text-twitterColor'
+                >
+                  <BsSearch className='w-4 h-4' title='Search Twitter'/>
+                </label>
+              </div>
+            </div>
+            <div className='flex flex-col bg-neutral-900 rounded-xl my-4 text-white'>
+              <h3 className='font-bold text-xl my-4 px-4'>What's happening?!</h3>
+              <div>
+                {Array.from({length:5}).map((_,i) => (
+                  <div
+                  key={i}
+                  className="hover:bg-white/10 p-4 last:rounded-b-xl transition duration-200"
+                  >
+                    <div className='font-bold text-lg'>Trending item {i + 1}</div>
+                    <div className='text-xs text-neutral-500'>35.4k</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className='flex flex-col bg-neutral-900 rounded-xl my-4 text-white'>
+              <h3 className='font-bold text-xl my-4 px-4'>Who to follow</h3>
+              <div>
+                {Array.from({length:5}).map((_,i) => (
+                  <div
+                  key={i}
+                  className="hover:bg-white/10 py-4 px-2 space-x-8 flex items-center last:rounded-b-xl transition duration-200"
+                  >
+                    <div className='flex flex-row space-x-2'>
+                      <div className='w-10 h-10 bg-neutral-600 rounded-full'></div>
+                      <div className='flex flex-col'>
+                        <div className='font-bold'>Other user</div>
+                        <div className='text-xs text-neutral-500'>@otheruser32</div>
+                      </div>
+                    </div>
+                    <div>
+                      <button className='rounded-full px-6 py-2 bg-white text-neutral-950 '>
+                        Follow
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className='py-4 px-2'>
+                <a className='text-twitterColor' href='#'>Show more</a>
+              </div>
+            </div>
+          </section>
+  )
+}
+
+export default RightSideBar
